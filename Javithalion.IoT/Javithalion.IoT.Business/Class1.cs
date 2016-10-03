fcc2a93c23a5424f4b0285e56c8bb2227bae7b22
javithalion <javithalion@DESKTOP-9GS6M7G>
2016-10-03 19:53:48 +0200

    Very first commit, solution structure

---
+++ Javithalion.IoT/Javithalion.IoT.Business/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Javithalion.IoT.Business
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
---
+++ Javithalion.IoT/Javithalion.IoT.DataAccess/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Javithalion.IoT.DataAccess")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("34cf5cff-9e59-45d0-bbfc-6a226111e8eb")]
