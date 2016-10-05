26910f812baa44ac836e3b3f882e6515b86a95be
Javithalion <javier_cervera_cano@hotmail.com>
2016-10-05 11:53:34 +0200

    Fixed solution structure and naming

---
+++ Javithalion.IoT/Javithalion.IoT.DeviceEvents.Business/ReadModel/IDeviceEventReadService.cs
@@ -0,0 +1,13 @@
+﻿using Javithalion.IoT.DeviceEvents.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Javithalion.IoT.DeviceEvents.Business.ReadModel
+{
+    interface IDeviceEventReadService
+    {
+        Task<IList<DeviceEvent>> FindAllAsync();
+    }
+}
---
+++ Javithalion.IoT/Javithalion.IoT.DeviceEvents.Domain/Entities/DeviceEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Javithalion.IoT.DeviceEvents.Domain.Entities
+{
+    public class DeviceEvent
+    {
+    }
+}
